3ae76d90760d0451697d66f8d3326d612442b652
Karen Calderon <krcatari@umsa.bo>
2026-06-11 12:29:57 -0400

    aplicando enviroments y paths

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Find the best gifs in the world',
+
+  //Api keys
+
+  //urls de referencias
+
+};
