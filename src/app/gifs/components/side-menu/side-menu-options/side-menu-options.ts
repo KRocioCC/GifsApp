ceb1c757ae40d5c334bd1e71facfe482c80fab28
Karen Calderon <krcatari@umsa.bo>
2026-06-11 12:06:20 -0400

    aplicando Roterlinks en menu de opciones para navegacion correcta

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,32 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  label: string;
+  route: string;
+  subLabel: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export default class SideMenuOptions {}
+export default class SideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      label: 'Search',
+      route: '/dashboard/search',
+      subLabel: 'Buscar gifs',
+      icon: 'fa-solid fa-magnifying-glass',
+    },
+    {
+      label: 'Trending',
+      route: '/dashboard/trending',
+      subLabel: 'Gifs Populares',
+      icon: 'fa-solid fa-chart-line',
+    },
+  ];
+
+}
